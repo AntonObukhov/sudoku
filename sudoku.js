40c742c7dd47c876717b14dddd765e14a93f3ad8
SofiyaAndreevna <sofiyazhirnova97@gmail.com>
2023-04-16 16:03:59 +0300

    Start solve function

--- sudoku.js
+++ sudoku.js
@@ -2,7 +2,7 @@ const { log } = require('console');
 const fs = require('fs');
 const boardString = fs.readFileSync('./puzzles.txt', 'utf-8');
 
-// делает массив из текстового файла судоку, только первый!!!!!!!!!!!1
+// делает массив из текстового файла судоку, только первый!!!!!!!!!!!(пока)
 function createArrFromText(boardString) {
   let sudokuArr1 = boardString.slice(0, 81).split('');
   let res = [];
@@ -24,9 +24,12 @@ function createArrFromText(boardString) {
   return res;
 }
 
+// присваиваем полученному массиву имя переменной board
+let board = createArrFromText(boardString);
+
 // возвращает координату пустого значения, если его нет возвращает null
-function checkEmpty() {
-  let sudocuFromText = createArrFromText(boardString);
+function checkEmpty(board) {
+  let sudocuFromText = board;
   for (let r = 0; r < sudocuFromText.length; r++) {
     for (let c = 0; c < sudocuFromText[r].length; c++) {
       if (sudocuFromText[r][c] === '-') {
@@ -36,14 +39,40 @@ function checkEmpty() {
   }
   return null;
 }
+// console.log(checkEmpty(board));
+let coordinatesOfEmpty = checkEmpty(board);
 
 /**
- * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
+ * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt. ---> у нас принимает массив board
  * Возвращает игровое поле после попытки его решить.
- * Договорись со своей командой, в каком формате возвращать этот результат.
+ * Договорись со своей командой, в каком формате возвращать этот результат - в формате массива
  */
-function solve(boardString) {}
 
+function solve(board) {
+  // когда возвращает false и рекурсия заканчивается
+  if (coordinatesOfEmpty === null){
+    return false;
+  } 
+
+  if (solve()){
+    // **currNumPosit позиция пустого элемента
+    let currNumPosit = coordinatesOfEmpty;
+    // начинаем подставлять числа от 1 до 9
+    for (let i = 1; i <= 9; i++) {
+      
+      // board.currNumPosit = i;
+      if (isValid){
+        //ЗДЕСЬ НЕПРАВИЛЬНО ПРИСВОИЛА КООРДИНАТУ ИЗМЕНЯЕМОГО ЧИСЛА(деструктуризация)
+        board.currNumPosit = i;
+      }
+      
+      
+    }
+  }
+  
+  return true;
+}
+// return board;
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
